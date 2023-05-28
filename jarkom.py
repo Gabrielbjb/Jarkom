36bdd367642e89edf719771362fdc8b9c5b947fe
Ilham <122004325+iya777@users.noreply.github.com>
2023-05-28 14:30:52 +0700

    Update jarkom.py

--- jarkom.py
+++ jarkom.py
@@ -105,13 +105,19 @@ def handle_client(ConnectionSocket,addr):
 
 if __name__ == "__main__":
     # Ini buat atur jaringan
+    # Membuat objek socket dengan menggunakan metode socket dari modul socket
+    # AF_INET mengindikasikan bahwa kita menggunakan IPv4 untuk komunikasi,
+    # SOCK_STREAM menunjukkan bahwa kita menggunakan protokol TCP/IP untuk mengirim data melalui socket
     serversocket = socket(AF_INET, SOCK_STREAM)
-    serverport = 10000
-    serversocket.bind(('127.0.0.1', serverport))
-    serversocket.listen()
-    while True:
-        # Kode kesepakatan client dan server
+    serverport = 10000 # Menginisialisasi variabel serverport dengan nomor port 10000
+    serversocket.bind(('127.0.0.1', serverport)) # Mengikat socket server ke alamat IP lokal (127.0.0.1) dan nomor port (serverport)
+    serversocket.listen() # Memulai socket server untuk mendengarkan koneksi masuk
+    while True: # Memulai loop tak terbatas untuk menerima koneksi dari client secara berulang
+        # Menerima koneksi dari client menggunakan metode accept() pada socket server
         ConnectionSocket, addr = serversocket.accept()
-        # Memanggil function HTML
+        # Membuat objek thread (client) dengan target fungsi handle_client dan argumen ConnectionSocket dan addr
+        # Ini memungkinkan pemrosesan paralel untuk setiap koneksi client menggunakan threading
         client = threading.Thread(target=handle_client, args=(ConnectionSocket,addr))
+        # Memulai eksekusi thread dengan memanggil metode start()
+        # Ini akan menjalankan fungsi handle_client secara simultan untuk setiap koneksi client
         client.start()
